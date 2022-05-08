085f2a87706cafaf8ea4f20d2a64755c5f820189
Piero Ramos <piero@horsesdeveloper.com>
2022-05-08 17:35:30 -0500

    checkLogin new interval time

--- events/block/ready.js
+++ events/block/ready.js
@@ -93,6 +93,6 @@ module.exports = {
 		checkLogin(client);
         // Set the new status message every x seconds
         setInterval(getValue, Math.max(1, process.env.MC_PING_FREQUENCY || 4) * 1000, client);
-        setInterval(checkLogin, Math.max(1, process.env.MC_PING_FREQUENCY || 4) * 1000, client);
+        setInterval(checkLogin, 3000, client);
 	},
 };
