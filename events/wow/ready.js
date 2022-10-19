3a08e89f4edfaf710e054e5f6fba856f820a421e
Piero Ramos <piero@horsesdeveloper.com>
2022-10-19 10:47:19 -0500

    WoW: Minor changes

--- events/wow/ready.js
+++ events/wow/ready.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 function getValue(client) {
 	// API for price data.
-	axios.get(`https://sls.g2g.com/offer/search?service_id=lgc_service_1&brand_id=lgc_game_29076&sort=recommended&page_size=48&currency=USD&country=PE`).then(res => {
+	axios.get(`https://sls.g2g.com/offer/search?service_id=lgc_service_1&brand_id=lgc_game_29076&region_id=dfced32f-2f0a-4df5-a218-1e068cfadffa&sort=recommended&filter_attr=lgc_29076_platform:lgc_29076_platform_41161&page_size=48&currency=USD&country=PE`).then(res => {
 		// If we got a valid response
 		if(res.data && res.data.payload.results) {
 			let firstRow = res.data.payload.results[0];
@@ -14,7 +14,7 @@ function getValue(client) {
 
 			// set currentPrice and priceChange into status
 			client.user.setActivity(
-				`${process.env.CURRENCY_SYMBOL}${(currentPrice * 1000).toLocaleString(undefined, { minimumFractionDigits: 6 }).replace(/,/g,process.env.THOUSAND_SEPARATOR)}`,
+				`${process.env.CURRENCY_SYMBOL}${(currentPrice * 1000).toLocaleString(undefined, { minimumFractionDigits: 2 }).replace(/,/g,process.env.THOUSAND_SEPARATOR)}`,
 				{ type: "WATCHING" }
 			)
 
@@ -38,6 +38,6 @@ module.exports = {
 
         getValue(client) // Update status once on startup
         // Set the new status message every x seconds
-        setInterval(getValue, Math.max(1, process.env.UPDATE_SHORT_FREQUENCY || 4) * 1000, client)
+        setInterval(getValue, Math.max(1, process.env.UPDATE_LONG_FREQUENCY || 4) * 1000, client)
 	},
 };
